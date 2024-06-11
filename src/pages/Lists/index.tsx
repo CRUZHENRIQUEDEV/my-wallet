612fc3034089713b16979bd8afb59edc72b71339
Henrique da Cruz Silva <henriquecruz.dev@gmail.com>
2024-06-11 00:54:21 -0300

    Test1

---
+++ src/pages/Lists/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// import { Container } from './styles';
+const List: React.FC = () => {
+  return (
+    <div>
+      <h1>List</h1>
+
+      <p>Teste</p>
+    </div>
+  );
+};
+export default List;
